5b3d6e47991cd3791d27da3c38e17afa84d2b4de
elie <cadeaudeelie@gmail.com>
2018-02-28 14:16:38 +0100

    ajout d'une boucle d'évenement

--- src/main.c
+++ src/main.c
@@ -2,20 +2,29 @@
 
 int main(int argc, char **argv)
 {
+	SDL_Window              *wind;
+	SDL_Event                evt;
+	int                      run = 1;
 	/* Initialisation simple */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n",
 						SDL_GetError()), exit(EXIT_FAILURE);
 	/* Création de la fenêtre */
-	SDL_Window              *pWindow = NULL;
-	pWindow =
-		SDL_CreateWindow("Démineur", SDL_WINDOWPOS_UNDEFINED,
-										 SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-	if (pWindow)
+	wind = NULL;
+	wind = SDL_CreateWindow("Démineur", SDL_WINDOWPOS_UNDEFINED,
+													SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	if (wind)
 	{
-		/* Attendre trois secondes, que l'utilisateur voit la fenêtre */
-		SDL_Delay(3000);
-		SDL_DestroyWindow(pWindow);
+		while (run)
+		{
+			SDL_WaitEvent(&evt);
+			switch (evt.type)
+			{
+			case SDL_QUIT:
+				run = 0;
+			}
+		}
+		SDL_DestroyWindow(wind);
 	}
 	else
 		fprintf(stderr, "Erreur de création de la fenêtre: %s\n",
